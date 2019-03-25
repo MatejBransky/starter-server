eb648f99bd9e238278b216777f2201a4be1c9ac3
Matej Bransky <matej.bransky@gmail.com>
2019-03-25 23:47:35 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+    es6: true,
+  },
+};
